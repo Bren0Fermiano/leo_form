75ad8ba767052e1c155355560eba6018c9e61c51
Bren0Fermiano <brenosantos.fermiano1@gmail.com>
2026-09-15 15:22:27 -0300

    some big changes and additions

--- src/main/java/form/com/leo_form/LeoFormApplication.java
+++ src/main/java/form/com/leo_form/LeoFormApplication.java
@@ -1,8 +1,11 @@
 package form.com.leo_form;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +13,53 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class LeoFormApplication {
 
+public class LeoFormApplication {
+ 		private static String url="jdbc:h2:./form;AUTO_SERVER=TRUE";
+		private static String user ="lf";
+		private static String key = "datakey";
+		
 	public static void main(String[] args) {
 		SpringApplication.run(LeoFormApplication.class, args);
+
 		
+		
+		try (Connection con  = DriverManager.getConnection(url,user,key);
+			Statement statement = con.createStatement(); ){
+			
+		     String sql = "CREATE TABLE IF NOT EXISTS alunos (" +
+                     "nome VARCHAR(100), " +
+                     "sobrenome VARCHAR(100), " +
+                     "curso VARCHAR(100), " +
+                     "data_nasc VARCHAR(20), " +
+                     "cpf VARCHAR(14), " +
+                     "cep VARCHAR(10), " +
+                     "telefone_a VARCHAR(15), " +
+                     "telefone_b VARCHAR(15), " +
+                     "telefone_fixo VARCHAR(15), " +
+                     "email VARCHAR(100))";
+
+						 statement.executeUpdate(sql);
+
+		} catch (Exception e) {
+			  System.out.println("Erro ao criar o banco: " + e.getMessage());
+		}
+  
+
+	
 	}
 
+    public String  getUrl(){
+       return url;
+	}
+
+	 public String  getUser(){
+       return user;
+	}
+
+	 public String  getKey(){
+       return key;
+	}
 }
 //.\mvnw.cmd spring-boot:run
 //.\ngrok.exe http 8080
--- src/main/java/form/com/leo_form/controler/envControler.java
+++ src/main/java/form/com/leo_form/controler/envControler.java
@@ -1,6 +1,12 @@
 package form.com.leo_form.controler;
-
+import form.com.leo_form.LeoFormApplication;
 import form.com.leo_form.env;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +17,7 @@ public class envControler {
     @PostMapping("/enviar")
     public String process(@ModelAttribute env form) {
         
+        LeoFormApplication lf = new  LeoFormApplication();
         // Print de verificação no terminal/console do Spring
         System.out.println("==========================================");
         System.out.println("      DADOS RECEBIDOS DO FORMULÁRIO       ");
@@ -26,11 +33,42 @@ public class envControler {
         System.out.println("E-mail        : " + form.getEmail());
         System.out.println("==========================================");
 
+
+
+        
+        String sql = "INSERT INTO alunos (nome, sobrenome, curso, data_nasc, cpf, cep, telefone_a, telefone_b, telefone_fixo, email) " +
+                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+       
+        	
+		try (Connection con  = DriverManager.getConnection(lf.getUrl(),lf.getUser(),lf.getKey());
+			  PreparedStatement stmt = con.prepareStatement(sql);){
+			
+            stmt.setString(1, form.getName());
+            stmt.setString(2, form.getLastname());
+            stmt.setString(3, form.getCourse());
+            stmt.setString(4, form.getDate());
+            stmt.setString(5, form.getCpf());
+            stmt.setString(6, form.getCep());
+            stmt.setString(7, form.getTelA());
+            stmt.setString(8, form.getTelB());
+            stmt.setString(9, form.getTelF());
+            stmt.setString(10, form.getEmail());
+			 stmt .executeUpdate();
+
+		} catch (Exception e) {
+			  System.out.println("Erro ao criar o banco: " + e.getMessage());
+		}
+	 
+    
         // Retorno exibido na tela do navegador ao enviar
         return "Formulário enviado com sucesso!<br><br>" +
                "<b>Nome:</b> " + form.getName() + " " + form.getLastname() + "<br>" +
                "<b>Curso:</b> " + form.getCourse() + "<br>" +
                "<b>E-mail:</b> " + form.getEmail() + "<br>" +
                "<b>CPF:</b> " + form.getCpf();
+
+             
     }
+
+
 }
