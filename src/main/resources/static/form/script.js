aa2aa3316c85b28259737ca2debdca279160b25f
Bren0Fermiano <brenosantos.fermiano1@gmail.com>
2026-09-20 22:47:00 -0300

    another things(strange things)

--- src/main/resources/static/form/script.js
+++ src/main/resources/static/form/script.js
@@ -1,17 +1,76 @@
 
 const cpfInpt = document.getElementById('cpf');
+if (cpfInpt) {
+  cpfInpt.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '').substring(0, 11);
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})\.(\d{3})(\d)/, '$1.$2.$3');
+    value = value.replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4');
+    e.target.value = value;
+  });
+}
 
-cpfInpt.addEventListener('input', (e) => {
-  let value = e.target.value;
-  value = value.replace(/\D/g, '');
-  value = value.substring(0, 11);
-  value = value.replace(/(\d{3})(\d)/, '$1.$2');
-  value = value.replace(/(\d{3})\.(\d{3})(\d)/, '$1.$2.$3');
-  value = value.replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4');
+const cepInpt = document.getElementById('cep');
+if (cepInpt) {
+  cepInpt.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '').substring(0, 8);
+    value = value.replace(/^(\d{5})(\d)/, '$1-$2');
+    e.target.value = value;
+  });
 
+  cepInpt.addEventListener('blur', (e) => {
+    const cleanCep = e.target.value.replace(/\D/g, '');
+    if (cleanCep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cleanCep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+
+        })
+     
+    }
+  });
+}
+
+function applyPhoneMask(e) {
+  let value = e.target.value.replace(/\D/g, '').substring(0, 11);
+  value = value.replace(/^(\d{2})(\d)/g, '($1) $2');
+  value = value.replace(/(\d{5})(\d)/, '$1-$2');
   e.target.value = value;
-});
+}
 
+const telAInpt = document.getElementById('telA');
+const telBInpt = document.getElementById('telfB');
+
+if (telAInpt) telAInpt.addEventListener('input', applyPhoneMask);
+if (telBInpt) telBInpt.addEventListener('input', applyPhoneMask);
+
+const telFInpt = document.getElementById('telfF');
+if (telFInpt) {
+  telFInpt.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '').substring(0, 10);
+    value = value.replace(/^(\d{2})(\d)/g, '($1) $2');
+    value = value.replace(/(\d{4})(\d)/, '$1-$2');
+    e.target.value = value;
+  });
+}
+
+const nisInpt = document.querySelector('input[name="nis"]');
+if (nisInpt) {
+  nisInpt.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '').substring(0, 11);
+    value = value.replace(/^(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/^(\d{3})\.(\d{5})(\d)/, '$1.$2.$3');
+    value = value.replace(/(\d{3})\.(\d{5})\.(\d{2})(\d)/, '$1.$2.$3-$4');
+    e.target.value = value;
+  });
+}
+
+const cgmInpt = document.getElementById('cgm');
+if (cgmInpt) {
+  cgmInpt.addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/\D/g, '').substring(0, 9);
+  });
+}
 
 const courseInpt = document.getElementById('selectC');
 const turnInpt = document.getElementById('selectT');
